a8a34b5f51546c7dec4efec8e3c3a90b2221b166
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-18 17:44:37 -0600

    mockobjects: refactoring using variables

--- mockobjects_test/run_mockobjects.bat
+++ mockobjects_test/run_mockobjects.bat
@@ -1,8 +1,18 @@
-REM copy in files
-copy ..\res\mockobjects-0.6-core.jar
-copy ..\res\mockobjects-0.6-j1.4-j2ee1.2.jar
-copy ..\res\selenium-server-standalone-2.11.0.jar
 
-javac -cp %CLASSPATH%;mockobjects-0.6-core.jar;mockobjects-0.6-j1.4-j2ee1.2.jar;selenium-server-standalone-2.11.0.jar TestTempServlet.java TemperatureServlet.java
+set RESDIR=..\res
+set this_classpath=%this_classpath%;%RESDIR%\mockobjects-0.6-core.jar
+set this_classpath=%this_classpath%;%RESDIR%\mockobjects-0.6-j1.4-j2ee1.2.jar
+set this_classpath=%this_classpath%;%RESDIR%\selenium-server-standalone-2.11.0.jar
 
-java -cp %CD%;%CLASSPATH%;mockobjects-0.6-core.jar;mockobjects-0.6-j1.4-j2ee1.2.jar;selenium-server-standalone-2.11.0.jar org.junit.runner.JUnitCore TestTempServlet
+set this_javac_files=TestTempServlet.java TemperatureServlet.java
+set this_java_target=TestTempServlet
+
+javac -cp %this_classpath% %this_javac_files%
+
+java -cp %CD%;%this_classpath% org.junit.runner.JUnitCore %this_java_target%
+
+REM cleanup; littering is bad
+set RESDIR=
+set this_classpath=
+set this_java_target=
+set this_javac_files=
