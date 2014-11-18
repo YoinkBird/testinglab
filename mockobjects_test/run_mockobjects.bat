9dbe31d0ad2dce9cf77f1921e9297283395b9cdc
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-18 17:44:37 -0600

    mockobjects: moving jar files to common dir

--- mockobjects_test/run_mockobjects.bat
+++ mockobjects_test/run_mockobjects.bat
@@ -1,3 +1,8 @@
+REM copy in files
+copy ..\res\mockobjects-0.6-core.jar
+copy ..\res\mockobjects-0.6-j1.4-j2ee1.2.jar
+copy ..\res\selenium-server-standalone-2.11.0.jar
+
 javac -cp %CLASSPATH%;mockobjects-0.6-core.jar;mockobjects-0.6-j1.4-j2ee1.2.jar;selenium-server-standalone-2.11.0.jar TestTempServlet.java TemperatureServlet.java
 
 java -cp %CD%;%CLASSPATH%;mockobjects-0.6-core.jar;mockobjects-0.6-j1.4-j2ee1.2.jar;selenium-server-standalone-2.11.0.jar org.junit.runner.JUnitCore TestTempServlet
