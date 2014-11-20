145e91b2dca8e2560f8e7a98127d37bb102f3bbc
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-19 19:41:12 -0600

    selenium: refactor into subroutines

--- 5_selenium/APTTestingLab.java
+++ 5_selenium/APTTestingLab.java
@@ -10,36 +10,47 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 // explanation http://docs.seleniumhq.org/docs/03_webdriver.jsp#selenium-webdriver-api-commands-and-operations
 public class APTTestingLab {
+    private static WebDriver driver;
     public static void main(String[] args) throws Exception {
         // The Firefox driver supports javascript 
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         
         // Go to the APT Testing Lab home page
         driver.get("http://apt-public.appspot.com/testing-lab-login.html");
-        
-        // Enter the username
-        // usernames:  andy, bob and charley
-        // passwords: apple, bathtub, china
-        // TODO: warning! make sure both lists are same length
-        List<String> userNames = Lists.newArrayList("andy");
-        List<String> userPasswords = Lists.newArrayList("apple");
-        WebElement query = driver.findElement(By.name("userId"));
-        query.clear();
-        for (int index = 0; index < userNames.size(); index++){
-          String userName = userNames.get(index);
-          String userPass = userPasswords.get(index);
-          query.sendKeys(userName);
-          query = driver.findElement(By.name("userPassword"));
-          query.clear();
-          query.sendKeys(userPass);
-        }
 
-        // "press enter"
-        query.submit();
+        // test different user logins
+        testLogins();
+        
+
 
         // Check the title of the page
         //System.out.println("Page title is: " + driver.getTitle());
 
         driver.quit();
+    }
+
+    private static void testLogins(){
+      // Enter the username
+      // usernames:  andy, bob and charley
+      // passwords: apple, bathtub, china
+      // TODO: warning! make sure both lists are same length
+      List<String> userNames = Lists.newArrayList("andy");
+      List<String> userPasswords = Lists.newArrayList("apple");
+      for (int index = 0; index < userNames.size(); index++){
+        String userName = userNames.get(index);
+        String userPass = userPasswords.get(index);
+        login(userName, userPass);
+      }
+    }
+
+    private static void login(String userName,String userPass){
+      WebElement query = driver.findElement(By.name("userId"));
+      query.clear();
+      query.sendKeys(userName);
+      query = driver.findElement(By.name("userPassword"));
+      query.clear();
+      query.sendKeys(userPass);
+      // "press enter"
+      query.submit();
     }
 }
