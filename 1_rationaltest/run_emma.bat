731f6283ff740541d2db2555e72c2947d8d27d3f
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-22 12:06:28 -0600

    emma: updating run script

--- 1_rationaltest/run_emma.bat
+++ 1_rationaltest/run_emma.bat
@@ -1,4 +1,30 @@
+set RESDIR=..\res
+set this_classpath=%this_classpath%;%RESDIR%\junit\4.10\junit-4.10.jar
+set this_classpath=%this_classpath%;%RESDIR%\emma.jar
+
+set this_classname=RationalTest
+set this_javac_files=%this_classname%.java
+set this_java_target=%this_classname%
+echo %this_classname%
+set this_classname=Rational
+set this_javac_files=%this_javac_files% %this_classname%.java
+set this_java_target=%this_java_target% %this_classname%
+echo %this_classname%
+
+REM remove class files, verify by listing dir contents
+del *class
+dir
+
 REM https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=747027#10
-set PWD=%CD%
-javac -source 1.6 -target 1.6 Rational.java RationalTest.java
-java -cp emma.jar emmarun -sp $PWD -r html -cp %CLASSPATH% RationalTest
+set this_javac_options=-source 1.6 -target 1.6
+javac %this_javac_options% -cp %this_classpath% %this_javac_files%
+
+java -cp %this_classpath% emmarun -sp %CD% -r html -cp %CD%;%this_classpath% %this_java_target%
+
+REM cleanup; littering is bad
+set RESDIR=
+set this_classpath=
+set this_classname=
+set this_java_target=
+set this_javac_files=
+set this_javac_options=
