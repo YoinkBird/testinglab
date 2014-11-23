cbe3f8099798e5f1e45bc48c34bdb226ca1d13f1
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-22 18:09:30 -0600

    easymock: adding stub MockMp3Player
    initial implementation, no compile errors

---
+++ 4_easymock/MockMp3Player.java
@@ -0,0 +1,95 @@
+// Philosophy of mockobjects:
+// // this file is a Mock Implementation to replace a real "Mp3Player" class for testing
+
+/**
+ * Excerpted from the book, "Pragmatic Unit Testing"
+ * ISBN 0-9745140-1-2
+ * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
+ * Visit www.PragmaticProgrammer.com
+ */
+
+import java.util.ArrayList;
+
+public class MockMp3Player implements Mp3Player {
+
+  // not playing by default
+  private boolean isPlaying = false;
+  // starts at beginning
+  private double currentPosition = 0.0;
+  // TODO: set this somewhere
+  private String currentSong = "";
+
+  /** 
+   * Begin playing the filename at the top of the
+   * play list, or do nothing if playlist 
+   * is empty. 
+   */
+  public void play(){
+  }
+
+
+  /** 
+   * Pause playing. Play will resume at this spot. 
+   */
+  public void pause(){
+  }
+
+
+  /** 
+   * Stop playing. The current song remains at the
+   * top of the playlist, but rewinds to the 
+   * beginning of the song.
+   */
+  public void stop(){
+  }
+
+  
+  /** Returns the number of seconds into 
+   * the current song.
+   */
+  public double currentPosition(){
+    return currentPosition;
+  }
+
+
+
+  /**
+   * Returns the currently playing file name.
+   */
+  public String currentSong(){
+    return currentSong;
+  }
+
+
+  /** 
+   * Advance to the next song in the playlist 
+   * and begin playing it.
+   */
+  public void next(){
+  }
+
+
+  /**
+   * Go back to the previous song in the playlist
+   * and begin playing it.
+   */
+  public void prev(){
+  }
+
+
+  /** 
+   * Returns true if a song is currently 
+   * being played.
+   */
+  public boolean isPlaying(){
+    return this.isPlaying;
+  }
+
+
+  /**
+   * Load filenames into the playlist.
+   */
+  public void loadSongs(ArrayList names){
+  }
+
+}
