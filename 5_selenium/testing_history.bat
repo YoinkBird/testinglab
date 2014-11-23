ee117059310e17ae42a812ef39b63ee0d8973958
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-22 22:11:37 -0600

    deliverables: selenium - script for video
    This script checks out each commit and runs the build,
    thereby demonstrating the evolution of the test.
    
    This can then be video-recorded for the video requirement

---
+++ 5_selenium/testing_history.bat
@@ -0,0 +1,66 @@
+git checkout 7b888b4
+run_selenium.bat
+del *class
+git checkout ed8a730
+run_selenium.bat
+del *class
+git checkout bfd2558
+run_selenium.bat
+del *class
+git checkout 560d936
+run_selenium.bat
+del *class
+git checkout 37bc1bf
+run_selenium.bat
+del *class
+git checkout 3b9f83e
+run_selenium.bat
+del *class
+git checkout 8c55110
+run_selenium.bat
+del *class
+git checkout 05605de
+run_selenium.bat
+del *class
+git checkout 765fad1
+run_selenium.bat
+del *class
+git checkout 8b7dd87
+run_selenium.bat
+del *class
+git checkout 6410340
+run_selenium.bat
+del *class
+git checkout cc23724
+run_selenium.bat
+del *class
+git checkout bdb6330
+run_selenium.bat
+del *class
+git checkout a81419f
+run_selenium.bat
+del *class
+git checkout f51c209
+run_selenium.bat
+del *class
+git checkout 9e328b8
+run_selenium.bat
+del *class
+git checkout 1321beb
+run_selenium.bat
+del *class
+git checkout 594ffc3
+run_selenium.bat
+del *class
+git checkout fd323bd
+run_selenium.bat
+del *class
+git checkout 3254749
+run_selenium.bat
+del *class
+git checkout f4b7d49
+run_selenium.bat
+del *class
+git checkout d3ec14b
+run_selenium.bat
+del *class
