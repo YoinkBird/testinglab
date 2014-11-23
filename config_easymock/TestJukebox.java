dad537f8c2d29d07e2e1a345a2f8eb8ddc6da485
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-22 18:09:30 -0600

    easymock setup: added explanation from documentation

--- config_easymock/TestJukebox.java
+++ config_easymock/TestJukebox.java
@@ -15,6 +15,15 @@ public class TestJukebox extends TestCase {
   }
 
   public void testEasyMockDemo() {
+    /* 
+       src: http://www.vogella.com/tutorials/EasyMock/article.html
+       EasyMock has several methods which are used to configure the Mock object. 
+       The expect() method tells EasyMock to simulate a method with certain arguments
+       The andReturn() method defines the return value of this method for the specified method parameters
+       The times() method defines how often the Mock object will be called
+    */
+
+    // http://easymock.org/user-guide.html#verification-return
     expect(mockJukebox.getCurrentSong()).andReturn("King Crimson -- Epitaph");
     // set up the mock object by calling methods you want to exist
     replay( mockJukebox_control );
